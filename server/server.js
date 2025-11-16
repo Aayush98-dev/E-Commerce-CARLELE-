63796a36c46d3ad210dd7c87e3eb12d72a49441f
Ayush Singh <singhaayushrajpoot4567@gmail.com>
2025-11-16 19:55:11 +0530

    commmit

--- server/server.js
+++ server/server.js
@@ -29,4 +29,5 @@ app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT ||  3000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))  
+// app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))  
+export default serverless(app);
